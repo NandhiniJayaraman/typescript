0dca7075414912e1c56e8ff4ce4559b29aa6140e
NandhiniJayaraman <nansjayaraman@gmail.com>
2023-09-21 12:55:40 +0530

    typrscript excersise

--- array.js
+++ array.js
@@ -27,4 +27,40 @@ console.log(divide({ dividend: 10, divisor: 2 }));
 // TS casting
 let x = 'hello';
 console.log(x.length);
-// class
+// simple class
+class Myclass {
+    constructor(x) {
+        this.a = x;
+    }
+}
+let o = new Myclass(34);
+console.log(o.a);
+// class implements interfaces
+class Person {
+    constructor(name, id) {
+        this.getname = () => {
+            return this.name;
+        };
+        this.name = name;
+        this.id = id;
+    }
+}
+let ob2 = new Person('nandhini', 123);
+console.log(ob2.id);
+console.log(ob2.getname());
+// // get and set value
+// class Myclass2{
+//      private _val:string='20'
+//      get nans():string{
+//         console.log('get')
+//         return this._val;
+//      }
+//      set nans(a1:any){
+//         console.log('set');
+//         this._val=a1
+//      }
+//     }
+// let o1=new Myclass2()
+// console.log(o1.nans);
+// o1.nans='50'
+// console.log(o1.nans);
--- array.ts
+++ array.ts
@@ -35,7 +35,63 @@ console.log(divide({dividend:10,divisor:2}))
 // TS casting
 let x:unknown = 'hello';
 console.log((x as string).length)
-// class
+// simple class
+class Myclass{
+    a:number
+    constructor(x:number){
+        this.a=x
+    }
+}
+let o=new Myclass(34)
+console.log(o.a)
+// interface
+interface inter1{
+    name:string
+    id:number
+}
+// class implements interfaces
+class Person implements inter1{
+    name:string
+    id:number
 
+    constructor(name:string,id:number){
+        this.name=name
+        this.id=id
+    }
+    getname=()=>{
+        return this.name
+    }
+}
+let ob2=new Person('nandhini',123)
+console.log(ob2.id);
+console.log(ob2.getname());
+
+
+
+
+
+
+
+
+
+// // get and set value
+// class Myclass2{
+//      private _val:string='20'
+//      get nans():string{
+//         console.log('get')
+//         return this._val;
+        
+//      }
+//      set nans(a1:any){
+//         console.log('set');
+//         this._val=a1
+//      }
+//     }
+// let o1=new Myclass2()
+// console.log(o1.nans);
+// o1.nans='50'
+// console.log(o1.nans);
+
+   
 
 
