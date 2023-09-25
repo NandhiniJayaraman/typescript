24992813b151a3c5f27d512d3b4ef941b4bbd2bd
NandhiniJayaraman <nansjayaraman@gmail.com>
2023-09-25 11:11:10 +0530

    calculator try

---
+++ calculator.js
@@ -0,0 +1,21 @@
+"use strict";
+function add(a, b) {
+    var c = a + b;
+    return c;
+}
+function sub(a, b) {
+    var c = a - b;
+    return c;
+}
+function mul(a, b) {
+    var c = a * b;
+    return c;
+}
+function div(a, b) {
+    var c = a / b;
+    return c;
+}
+console.log(add(1, 2));
+console.log(sub(3, 4));
+console.log(mul(5, 6));
+console.log(div(1, 2));
---
+++ calculator.ts
@@ -0,0 +1,20 @@
+function add(a:number,b:number):number{
+    var c=a+b
+    return c
+}
+function sub(a:number,b:number):number{
+    var c=a-b
+    return c
+}
+function mul(a:number,b:number):number{
+    var c=a*b
+    return c
+}
+function div(a:number,b:number):number{
+    var c=a/b
+    return c
+}
+console.log(add(1,2))
+console.log(sub(3,4))
+console.log(mul(5,6))
+console.log(div(1,2))
