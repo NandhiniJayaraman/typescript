53a320d03f79acd78293870195ce477268828be3
NandhiniJayaraman <nansjayaraman@gmail.com>
2023-09-27 16:37:10 +0530

    new calculator without eval

--- intract.js
+++ intract.js
@@ -1,3 +1,42 @@
-function display(){
-    alert('hi everyone')
+function add(){
+    let  m=Number(document.getElementById("input1").value);
+    var n=Number(document.getElementById("input2").value);
+    c=m+n
+    console.log(c);
+    var resulr=document.getElementById("result")
+    resulr.innerHTML=c
+    
+}
+function sub(){
+    var m=Number(document.getElementById("input1").value);
+    var n=Number(document.getElementById("input2").value);
+   c=m-n
+   console.log(c);
+   var resulr=document.getElementById("result")
+   resulr.innerHTML=c
+}
+function mul(){
+    var m=Number(document.getElementById("input1").value);
+    var n=Number(document.getElementById("input2").value);
+   c=m*n
+   console.log(c);
+   var resulr=document.getElementById("result")
+   resulr.innerHTML=c
+}
+function div(){
+    var m=Number(document.getElementById("input1").value);
+    var n=Number(document.getElementById("input2").value);
+   c=m/n
+   console.log(c);
+   var resulr=document.getElementById("result")
+   resulr.innerHTML=c
+}
+function clea(){
+    var new1 =document.getElementById("input1");
+    new1.value=''
+    var new2 =document.getElementById("input2");
+    new2.value=''
+    var resulr= document.getElementById("result")
+    c=0
+    resulr.innerHTML=c
 }
